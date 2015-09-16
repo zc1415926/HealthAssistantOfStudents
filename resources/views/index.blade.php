c08ac99d81e8cb70345d34d2f3b20e331848d71f
zc1415926 <zc1415926@126.com>
2015-09-16 22:42:33 +0800

    修改主页路由及主页模板

---
+++ resources/views/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.deafult')
+    @section('content')
+        <h1>请输入你的信息</h1>
+    @endsection
+@stop
---
+++ resources/views/layouts/deafult.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>环肥燕瘦</title>
+
+    <link href="http://cdn.bootcss.com/bootstrap/2.3.2/css/bootstrap.min.css" rel="stylesheet">
+    <link href="http://cdn.bootcss.com/bootstrap/2.3.2/css/bootstrap-responsive.min.css" rel="stylesheet">
+
+</head>
+<body>
+<div class="container">
+    @yield('content')
+</div>
+<script src="http://cdn.bootcss.com/jquery/1.11.1/jquery.min.js"></script>
+<script src="http://cdn.bootcss.com/bootstrap/2.3.2/js/bootstrap.min.js"></script>
+</body>
+</html>
