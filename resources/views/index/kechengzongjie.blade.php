a0f60fdd5c574a668ed3efeb6dd3d8836736c666
zc1415926 <zc1415926@126.com>
2015-10-06 20:19:52 +0800

    调排版及网络留言地址

--- resources/views/index/kechengzongjie.blade.php
+++ resources/views/index/kechengzongjie.blade.php
@@ -2,23 +2,22 @@
 @section('content')
     <header class="jumbotron subhead" id="overview">
         <div class="container">
-            <h1>课程总结</h1>
+            <h1>一统江湖</h1>
 
         </div>
     </header>
 
     <div class="container">
-        <div class="marketing">
-            <h1>总结项目</h1>
-            <p class="marketing-byline">1.本课知识点。练会各级武功即可。</p>
-            <p class="marketing-byline">2.树立正确的健康观念，随时关注自己的BMI值。<br />做到膳食均衡，不挑食、不偏食；积极参加各种体育活动。</p>
-            <p class="marketing-byline">3.填写网络问卷。</p>
-            <div class="row-fluid">
-                <div class="span12">
-                    <a href="http://www.sojump.com/jq/3968793.aspx">点我“填写网络问卷”</a>
+        <div class="row">
+            <div class="span6 offset3">
+                <div class="page-header">
+                    <h1>总结项目</h1>
                 </div>
-            </div>
+                <p>1. 本课知识点。练会各级武功即可。</p>
 
-        </div>
+                <p>2. 树立正确的健康观念，随时关注自己的BMI值。做到膳食均衡，不挑食、不偏食；积极参加各种体育活动。</p>
+
+                <p>3. <a href="/comment">点我“网络留言”</a></p>
+            </div>
     </div>
 @stop
