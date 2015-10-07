ec3daab8d0840f48be1c63ede7336a9b952410bd
zc1415926 <zc1415926@126.com>
2015-10-07 12:58:44 +0800

    修改“武林大会”图片及排版

--- resources/views/index/wulindahui.blade.php
+++ resources/views/index/wulindahui.blade.php
@@ -3,31 +3,22 @@
     <header class="jumbotron subhead" id="overview">
         <div class="container">
             <h1>武林大会</h1>
-
         </div>
     </header>
 
     <div class="container">
-        <div class="marketing">
-            <h1>大会议程</h1>
-            <p class="marketing-byline">1.判断自己和组内成员BMI值是否达标。</p>
-            <div class="row-fluid">
-                <div class="span12">
-                    <img src="img/bmi.png">
-                    <p class="marketing-byline"> </p>
+        <div class="row">
+            <div class="span6 offset3">
+                <div class="page-header">
+                    <h1>大会议程</h1>
                 </div>
-            </div>
-            <p class="marketing-byline">2.小组合作探讨未达标的原因，利用网络搜索答案等方法，提出如何达标的方案。</p>
-            <div class="row-fluid">
-                <div class="span12">
-                    <p>&nbsp;</p>
-                </div>
-            </div>
-            <p class="marketing-byline">3. 武林大会：在班级博客中公布达标方案。</p>
-            <div class="row-fluid">
-                <div class="span12">
-                    <a href="http://cqgs.aschool.cn/">点我进入“班级”博客</a>
-                </div>
+
+                <p class="lead">1. 判断自己和组内成员BMI值是否达标。</p>
+                <img src="img/bmi.jpg">
+
+                <p class="lead">2. 小组合作探讨未达标的原因，利用网络搜索答案等方法，提出如何达标的方案。</p></p>
+                <p class="lead">3. 武林大会：在班级博客中公布达标方案。</p>
+                <p class="lead">3. <a href="/comment">点我提交“达标方案”。</a></p>
             </div>
         </div>
     </div>
