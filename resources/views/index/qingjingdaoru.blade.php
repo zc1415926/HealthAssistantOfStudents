fee6b84c5c592bbbf9e4fb832ff4e202850b7fdf
ZhangChuan <zc1415926@126.com>
2015-10-08 15:21:35 +0800

    更换杨玉环和赵飞燕的顺序

--- resources/views/index/qingjingdaoru.blade.php
+++ resources/views/index/qingjingdaoru.blade.php
@@ -16,19 +16,19 @@
                 </div>
 
                 <p class="lead">
-                    &emsp;&emsp;话说当年汉朝皇后赵飞燕和唐朝贵妃杨玉环，穿越到现代，她们都觉得自己是大美人。但是现代人都觉得苗条的赵飞燕是大美女，而胖胖的杨玉环很难看，杨玉环发奋减肥，因减肥过度，送进了医院。
+                    &emsp;&emsp;话说当年唐朝贵妃杨玉环和汉朝皇后赵飞燕，穿越到现代，她们都觉得自己是大美人。但是现代人都觉得苗条的赵飞燕是大美女，而胖胖的杨玉环很难看，杨玉环发奋减肥，因减肥过度，送进了医院。
                 </p>
 
                 <div class="row-fluid">
-                    <div class="span5">
-                        <img class="" src="/img/shou.jpg">
-
-                        <h2 align="center">赵飞燕</h2>
-                    </div>
                     <div class="span5 offset2">
                         <img class="" src="/img/pang.jpg">
 
                         <h2 align="center">杨玉环</h2>
+                    </div>
+                    <div class="span5">
+                        <img class="" src="/img/shou.jpg">
+
+                        <h2 align="center">赵飞燕</h2>
                     </div>
                 </div>
             </div>
