0d92b671a1b1608df17800d88c2bfa1f1a2210fa
zc1415926 <zc1415926@126.com>
2015-10-11 11:29:42 +0800

    修改标题居中

--- resources/views/index/kechengzongjie.blade.php
+++ resources/views/index/kechengzongjie.blade.php
@@ -8,11 +8,12 @@
     </header>
 
     <div class="container">
+        <div class="marketing">
+            <h1>总结项目</h1>
+            <br>
+        </div>
         <div class="row">
             <div class="span6 offset3">
-                <div class="page-header">
-                    <h1>总结项目</h1>
-                </div>
                 <p class="lead">1. 本课知识点。练会各级武功即可。</p>
                 <p class="lead">2. 树立正确的健康观念，随时关注自己的BMI值。做到膳食均衡，不挑食、不偏食；积极参加各种体育活动。</p>
                 <p class="lead">3. <a href="http://www.sojump.com/jq/3968793.aspx">点我填写“网络问卷”</a>。</p>
--- resources/views/index/wulindahui.blade.php
+++ resources/views/index/wulindahui.blade.php
@@ -7,12 +7,12 @@
     </header>
 
     <div class="container">
+        <div class="marketing">
+            <h1>大会议程</h1>
+            <br>
+        </div>
         <div class="row">
             <div class="span8 offset2">
-                <div class="page-header">
-                    <h1>大会议程</h1>
-                </div>
-
                 <p class="lead">1. 判断自己和组内成员BMI值是否达标。</p>
             </div>
             <div class="span10 offset2">
