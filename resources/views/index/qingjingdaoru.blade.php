ace38bff811d28cd25a5deb847980d41cc89a72f
zc1415926 <zc1415926@126.com>
2015-10-11 11:25:37 +0800

    修改“风起云涌”排版至页内标题居中

--- resources/views/index/qingjingdaoru.blade.php
+++ resources/views/index/qingjingdaoru.blade.php
@@ -9,28 +9,27 @@
     </header>
 
     <div class="container">
-        <div class="row">
-            <div class="span8 offset2">
-                <div class="page-header">
-                    <h1>时空穿越</h1>
-                </div>
+        <div class="marketing">
+            <h1>时空穿越</h1>
+        </div>
+        <div class="span10 offset1">
+            <p class="lead">
+                <br>
+                &emsp;&emsp;话说当年唐朝贵妃杨玉环和汉朝皇后赵飞燕，穿越到现代，她们都觉得自己是大美人。但是现代人都觉得苗条的赵飞燕是大美女，而胖胖的杨玉环很难看，杨玉环发奋减肥，因减肥过度，送进了医院。
+            </p>
+        </div>
+        <div class="row-fluid span10 offset1">
+            <div class="span4">
+                <img class="" src="/img/pang.jpg">
 
-                <p class="lead">
-                    &emsp;&emsp;话说当年唐朝贵妃杨玉环和汉朝皇后赵飞燕，穿越到现代，她们都觉得自己是大美人。但是现代人都觉得苗条的赵飞燕是大美女，而胖胖的杨玉环很难看，杨玉环发奋减肥，因减肥过度，送进了医院。
-                </p>
+                <h2 align="center">杨玉环</h2>
+            </div>
+            <div class="span4 offset2">
+                <img class="" src="/img/shou.jpg">
 
-                <div class="row-fluid">
-                    <div class="span5">
-                        <img class="" src="/img/pang.jpg">
-
-                        <h2 align="center">杨玉环</h2>
-                    </div>
-                    <div class="span5 offset2">
-                        <img class="" src="/img/shou.jpg">
-
-                        <h2 align="center">赵飞燕</h2>
-                    </div>
-                </div>
+                <h2 align="center">赵飞燕</h2>
             </div>
         </div>
+
+    </div>
 @stop
