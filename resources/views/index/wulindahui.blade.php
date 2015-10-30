47029c6c0580a89ecc39029878837a5a149c1f14
zc1415926 <zc1415926@126.com>
2015-10-30 20:57:33 +0800

    视频配字幕，BMI达标“建议”改成“方案”

--- resources/views/index/wulindahui.blade.php
+++ resources/views/index/wulindahui.blade.php
@@ -10,10 +10,12 @@
         <div class="row">
             <div class="span10 offset2">
                 <br><br>
-                <p class="lead">1. 以小组为单位，判断组内同学的BMI值是否达标。</p>
-                <p class="lead">2. 探讨未达标的原因，并提出“如何达标”的建议。</p></p>
-                <p class="lead">3. 小组长将建议写在留言区，<a href="/comment">单击我留言</a>。</p>
+                <p class="lead">1. 以小组为单位，判断组内同学的序号所对应的BMI值是否达标。</p>
+                <p class="lead">2. 探讨未达标的原因，并提出“如何达标”的方案。</p></p>
+                <p class="lead">3. 小组长将方案写在留言区，<a href="/comment">单击我留言</a>。</p>
             </div>
         </div>
+        <br><br>
+        <br><br>
     </div>
 @stop
